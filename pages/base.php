f93859c0b4e8e169e46ae52c9d0a8f0a3c264e0f
lamcahng <alanlamchang@gmail.com>
2016-09-01 13:00:28 -0500

    Base php
    PHP diles to créate pages

---
+++ pages/base.php
@@ -0,0 +1,5 @@
+<?php include "header.php"; ?>
+
+<!-- YOUR HTML HERE -->
+
+<?php include "footer.php"; ?>
---
+++ pages/footer.php
@@ -0,0 +1,8 @@
+    <!-- SCRIPTS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="js/frontend.js"></script>
+    <script src="js/backend.js"></script>
+
+</body>
+
+</html>
