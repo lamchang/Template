dcd87abc9e3edd70ca9cf86c6e2bb025b1b2caad
lamcahng <alanlamchang@gmail.com>
2016-09-01 12:55:36 -0500

    First Push

---
+++ js/backend.js
@@ -0,0 +1,14 @@
+// VARIABLES
+
+// FUNCIONES
+
+function start(){
+}
+
+function responsive(){
+}
+
+// INVOCACIÓN
+
+$( document ).on( 'ready', start );
+$( window ).on( 'load resize', responsive );
